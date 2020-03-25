ad9445e2dd149d1c2f64d8e0a5d805a08fa58bc5
Jordan Melidor-Fuxis <jordan.melidor@hotmail.fr>
2020-03-25 17:04:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,7 +21,8 @@ def long_planeteer_calls(array)
   array.any? {|item| item.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
+  # code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
