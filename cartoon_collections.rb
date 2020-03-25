7f4c23b3cfe0823e50d757f10c6c834d48f52347
Jordan Melidor-Fuxis <jordan.melidor@hotmail.fr>
2020-03-25 17:00:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,8 +15,10 @@ def summon_captain_planet(array)
   }
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)
+  # code an argument here
   # Your code here
+  
 end
 
 def find_the_cheese# code an argument here
