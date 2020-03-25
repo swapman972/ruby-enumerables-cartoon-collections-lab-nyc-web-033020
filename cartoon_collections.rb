1062643a715b10d79fa99316194991ef7a596341
Jordan Melidor-Fuxis <jordan.melidor@hotmail.fr>
2020-03-25 16:48:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,9 @@
 def roll_call_dwarves(array)
   # code an argument here
   # Your code here
-  array.each_with_index {|item, i| puts "#{i} #{item}"}
+  array.each_with_index {|item, i| 
+  counter = i +1
+  puts "#{i} #{counter}"}
 end
 
 def summon_captain_planet# code an argument here
