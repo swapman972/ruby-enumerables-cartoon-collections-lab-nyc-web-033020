1381219c5eabd8b91db1017cf2785348f6ea76c6
Jordan Melidor-Fuxis <jordan.melidor@hotmail.fr>
2020-03-25 17:02:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,7 @@ end
 def long_planeteer_calls(array)
   # code an argument here
   # Your code here
-  array.any? {|item| item.
+  array.any? {|item| item.length > 4}
 end
 
 def find_the_cheese# code an argument here
