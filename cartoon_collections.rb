451bcd600c425c77f6e117aa0d4bdf851a8a1800
Jordan Melidor-Fuxis <jordan.melidor@hotmail.fr>
2020-03-25 16:49:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,10 @@ def roll_call_dwarves(array)
   puts "#{counter} #{item}"}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)
+  # code an argument here
   # Your code here
+  
 end
 
 def long_planeteer_calls# code an argument here
